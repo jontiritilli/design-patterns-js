7adfcc9b6f8688f7c74965dd0f228a351e920240
Jonathan Tiritilli <jonathantiritilli@gmail.com>
2019-05-02 10:32:20 -0700

    init commit, add readme, add Chain of Responsibility

---
+++ chain-of-responsibility.js
@@ -0,0 +1,55 @@
+class Account {
+  setNext(account) {
+    this.successor = account;
+  }
+
+  pay(amountToPay) {
+    if (this.canPay(amountToPay)) {
+      console.log(`Paid ${amountToPay} using ${this.name}`);
+    } else if (this.successor) {
+      console.log(`Cannot pay using ${this.name}. Proceeding...`);
+      this.successor.pay(amountToPay);
+    } else {
+      console.log('None of the accounts have enough balance');
+    }
+  }
+
+  canPay(amount) {
+    return this.balance >= amount;
+  }
+}
+
+class Bank extends Account {
+  constructor(balance) {
+    super();
+    this.name = 'bank';
+    this.balance = balance;
+  }
+}
+
+class Paypal extends Account {
+  constructor(balance) {
+    super();
+    this.name = 'Paypal';
+    this.balance = balance;
+  }
+}
+
+class Bitcoin extends Account {
+  constructor(balance) {
+    super();
+    this.name = 'bitcoin';
+    this.balance = balance;
+  }
+}
+(function test() {
+  const bank = new Bank(100); // Bank with balance 100
+  const paypal = new Paypal(200); // Paypal with balance 200
+  const bitcoin = new Bitcoin(300); // Bitcoin with balance 300
+
+  bank.setNext(paypal);
+  paypal.setNext(bitcoin);
+
+  // Let's try to pay using the first priority i.e. bank
+  bank.pay(259);
+})();
